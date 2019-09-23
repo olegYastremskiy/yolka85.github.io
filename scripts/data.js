7a0d54bfeac88013dd6feb2b53874eb19ebdce28
Yolka85 <4yolka@gmail.com>
2019-09-24 00:18:40 +0300

    Added scrolling, last section, script for slider

--- scripts/data.js
+++ scripts/data.js
@@ -1,109 +1,186 @@
 "use strict";
 var data = {
-    menu: [
-        {
-            id: 'about',
-            name: 'про що навчання',
-        },
-        {
-            id: 'speakers',
-            name: 'лектори',
-        },
-        {
-            id: 'apply',
-            name: 'подати заявку',
-        }
-    ],
-    mainTop: {
-        circle: 'apply till 28 october',
-        heading: 'Де взяти практику, якщо теорії вже вдосталь? Join PMBA!',
-        text: [
-            {
-                question: 'Що це?',
-                answer: 'Практичні знання від топ-менеджерів компанії.',
-            },
-            {
-                question: 'Навіщо?',
-                answer: 'Щоб отримати більше досвіду та розуміння, де ти хочеш працювати.',
-            },
-            {
-                question: 'В якому форматі?',
-                answer: 'Offline/Online воркшопи & лекції щосуботи.',
-            },
-            {
-                question: 'Коли?',
-                answer: 'З 2 листопада по 12 грудня',
-            },
-        ]
+  menu: [
+    {
+      id: "about",
+      name: "про що навчання"
     },
-    about: {
-        heading: 'Про що навчання?',
-        info: {
-            tile1: {
-                heading: 'Marketing',
-                emojiSrc: '../images/icon-black-heart.png',
-                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-            },
-            tile2: {
-                heading: 'Digital',
-                emojiSrc: '../images/icon-male-technologis.png',
-                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-            },
-            tile3: {
-                heading: 'Business Intelligence',
-                emojiSrc: '../images/icon-briefcase.png',
-                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-            },
-            tile4: {
-                heading: 'Sales',
-                emojiSrc: '../images/icon-money-mouth-face.png',
-                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-            },
-            tile5: {
-                heading: 'IT',
-                emojiSrc: '../images/icon-brain.png',
-                description: 'Внутрішні сайти, боти та підтримка кожного співробітника з його лептопом.',
-            },
-            tile6: {
-                heading: 'Finance',
-                emojiSrc: '../images/icon-money-with-wings.png',
-                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-            },
-            tile7: {
-                heading: 'P&C',
-                emojiSrc: '../images/icon-person-raising-both-hands-in-celebration.png',
-                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-            },
-        }
+    {
+      id: "speakers",
+      name: "лектори"
     },
-    speakers: {
-        title: 'Спікери',
-        info: [
-            {
-                image: 'https://i.photographers.ua/thumbnails/pictures/44911/240x2.jpg',
-                name: 'Дмитро Зінченко',
-                position: 'Head of Digital Ukraine, Caucasus & Moldova',
-            },
-            {
-                image: 'https://i.pinimg.com/originals/aa/01/39/aa0139b260f3edebb46dc1bd2e7a1117.jpg',
-                name: 'Lorem ipsum',
-                position: 'dolor sit amet, consectetur adipiscing elit, sed do eiusmod',
-            },
-            {
-                image: 'https://i0.wp.com/www.kirilltigai.com/wp-content/uploads/Muzhskoj-portret-v-studii_6.jpg?resize=1200%2C1800&ssl=1',
-                name: 'Lorem ipsum',
-                position: 'dolor sit amet, consectetur adipiscing elit, sed do eiusmod',
-            },
-            {
-                image: 'https://i.pinimg.com/originals/98/72/98/9872982f43e82b929487aea5d47c4ee7.jpg',
-                name: 'Lorem ipsum',
-                position: 'dolor sit amet, consectetur adipiscing elit, sed do eiusmod',
-            },
-            {
-                image: 'https://st2.depositphotos.com/1004384/5837/i/450/depositphotos_58373937-stock-photo-beautiful-blonde-girl-portrait-on.jpg',
-                name: 'Lorem ipsum',
-                position: 'dolor sit amet, consectetur adipiscing elit, sed do eiusmod',
-            },
-        ]
+    {
+      id: "apply",
+      name: "подати заявку"
     }
+  ],
+  mainTop: {
+    circle: "apply till 28 october",
+    heading: "Де взяти практику, якщо теорії вже вдосталь? Join PMBA!",
+    text: [
+      {
+        question: "Що це?",
+        answer: "Практичні знання від топ-менеджерів компанії."
+      },
+      {
+        question: "Навіщо?",
+        answer:
+          "Щоб отримати більше досвіду та розуміння, де ти хочеш працювати."
+      },
+      {
+        question: "В якому форматі?",
+        answer: "Offline/Online воркшопи & лекції щосуботи."
+      },
+      {
+        question: "Коли?",
+        answer: "З 2 листопада по 12 грудня"
+      }
+    ]
+  },
+  about: {
+    heading: "Про що навчання?",
+    info: {
+      tile1: {
+        heading: "Marketing",
+        emojiSrc: "images/icon-black-heart.png",
+        description:
+          "Це – не лише про 4P і Котлера. Приходь послухати, як насправді створюється реклама."
+      },
+      tile2: {
+        heading: "Digital",
+        emojiSrc: "images/icon-male-technologis.png",
+        description:
+          "E-commerce, боти, CRM-системи… І це далеко не все, чим ми тут займаємося."
+      },
+      tile3: {
+        heading: "Business Intelligence",
+        emojiSrc: "images/icon-briefcase.png",
+        description:
+          "Створити звіт, який зрозуміє навіть твоя бабуся – це до нас. Приходь, тобі також розповімо."
+      },
+      tile4: {
+        heading: "Sales",
+        emojiSrc: "images/icon-money-mouth-face.png",
+        description:
+          "Як побудований Sales Department у одній з найбільших компаній країни?"
+      },
+      tile5: {
+        heading: "IT",
+        emojiSrc: "images/icon-brain.png",
+        description:
+          "Внутрішні сайти, боти та підтримка кожного співробітника з його лептопом."
+      },
+      tile6: {
+        heading: "Finance",
+        emojiSrc: "images/icon-money-with-wings.png",
+        description: "Мабуть, найважливіший відділ у компанії."
+      },
+      tile7: {
+        heading: "P&C",
+        emojiSrc: "images/icon-person-raising-both-hands-in-celebration.png",
+        description:
+          "Як менеджити менеджерів? Мотивувати тисячі співробітників? Easy. Розповімо все на модулі People and Culture."
+      }
+    }
+  },
+  speakers: {
+    title: "Спікери",
+    info: [
+      {
+        image: "images/DmitriyZinchenko.png",
+        name: "Дмитро Зінченко",
+        position: "Digital Ukraine, Caucasus & Moldova"
+      },
+      {
+        image: "images/UliyanaKravchenko.png",
+        name: "Уляна Кравченко",
+        position: "Marketing"
+      },
+      {
+        image: "images/AndriySerov.png",
+        name: "Андрій Сєров",
+        position: "Marketing"
+      },
+      {
+        image: "images/RomanGoyaniyk.png",
+        name: "Роман Гоянюк",
+        position: "Sales"
+      },
+      {
+        image: "images/MichailMuller.png",
+        name: "Михайло Муллєр",
+        position: "Sales"
+      },
+      {
+        image: "images/ArtemDvoretskiy.png",
+        name: "Артем Дворецький",
+        position: "Business Intelligence"
+      },
+      {
+        image: "images/KonstantinBurdin.png",
+        name: "Костянтин Бурдін",
+        position: "IT"
+      },
+      {
+        image: "images/YuliyaMalko.png",
+        name: "Юлія Малько",
+        position: "People&Culture"
+      },
+      {
+        image: "images/OlenaKharhan.png",
+        name: "Олена Хархан",
+        position: "People&Culture"
+      },
+      {
+        image: "images/NataliyaVolosovich.png",
+        name: "Наталія Волосович",
+        position: "People&Culture"
+      },
+      {
+        image: "images/AndriyNazarov.png",
+        name: "Андрій Назаров",
+        position: "People&Culture"
+      },
+      {
+        image: "images/MariiaSokol.png",
+        name: "Марія Сокол",
+        position: "Supply Chain"
+      },
+      {
+        image: "images/AnnaBaboshko.png",
+        name: "Анна Бабошко",
+        position: "Procurement"
+      },
+      {
+        image: "",
+        name: "Оксана Мартиненко",
+        position: "Environment&Compliance"
+      },
+      {
+        image: "images/YuriyKhlopyanyk.png",
+        name: "Юра Хлопяник",
+        position: "Warehousing"
+      }
+    ]
+  },
+  location: {
+    title: "Що по географії?",
+    text: [
+      {
+        bold: "Київ & Харків",
+        regular: "– оффлайн у наших офісах."
+      },
+      {
+        bold:
+          "Полтава, Чернігів, Івано-Франківськ, Львів, Хмельницький, Дніпро, Запоріжжя, Миколаїв, Вінниця, Одеса",
+        regular: "– online хоч з Балі. Головне – май при собі лептоп."
+      },
+      {
+        bold: "P.S.",
+        regular:
+          "Перше, що ти побачиш в анкеті – це текст: Завантаж резюме. Без паніки, просто скроль нижче і заповнюй без CV."
+      }
+    ],
+    string: "Подати заявку"
+  }
 };
--- scripts/main.js
+++ scripts/main.js
@@ -1,87 +1,132 @@
-(function(){
-    "use strict";
+(function() {
+  "use strict";
 
-    createHTML(data);
+  createHTML(data);
 
-    function createHTML(context){
-        var template = document.getElementById("pmba").innerHTML,
-            templateCompile = Handlebars.compile(template),
-            templateHTML = templateCompile(context);
-            document.getElementById('app').innerHTML = templateHTML;
+  function createHTML(context) {
+    var template = document.getElementById("pmba").innerHTML,
+      templateCompile = Handlebars.compile(template),
+      templateHTML = templateCompile(context);
+    document.getElementById("app").innerHTML = templateHTML;
+  }
+
+  // Burger menu
+  $(".header__burger--trigger").click(function() {
+    $(".header__nav").slideDown(400, function() {
+      if ($(this).css("display") === "none") {
+        $(this).removeAttr("style");
+      }
+      $("#app").addClass("menu-open");
+    });
+  });
+  $(document).click(function(event) {
+    if ($(event.target).closest(".header__burger--trigger").length) return;
+    if ($(event.target).hasClass("header__nav")) return;
+    if ($(event.target).hasClass("header__nav--closer")) return;
+    if ($(".header__nav").css("display") === "block") {
+      $(".header__nav").removeAttr("style");
+      $("#app").removeClass("menu-open");
     }
+    event.stopPropagation();
+  });
 
-    // Burger menu
-    $('.header__burger--trigger').click(function(){
-        $('.header__nav').slideDown(400, function(){
-            if( $(this).css('display') === "none"){
-                $(this).removeAttr('style');
-            }
-            $('#app').addClass('menu-open');
-        });
-    });
-    $(document).click(function(event){
-        if ($(event.target).closest('.header__burger--trigger').length) return;
-        if ($(event.target).hasClass('header__nav')) return;
-        if ($(event.target).hasClass('header__nav--closer')) return;
-        if ($('.header__nav').css('display') === "block"){
-                $('.header__nav').removeAttr('style');
-                $('#app').removeClass('menu-open');
-            }
-        event.stopPropagation();
-    });
+  // Circle text
+  const circleType = new CircleType(
+    document.getElementById("main-top__img--circle-text")
+  );
+  circleType.radius();
 
-    // Circle text
-    const circleType = new CircleType(document.getElementById('main-top__img--circle-text'));
-    circleType.radius();
+  //About mobile tiles
+  $(".about__tile").click(function(event) {
+    $(event.target)
+      .parent()
+      .toggleClass("about__tile--selected");
+  });
 
-    //About mobile tiles
-    $('.about__tile').click(function(event){
-        $(event.target).parent().toggleClass( "about__tile--selected" );
-    });   
-    
-    //Slick slider
-    $('.speakers__carousel').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        centerMode: true,
-        centerPadding: '40px 0px 0px',
-        prevArrow: '<div class="slick-prev"><img class="speakers__carousel--btn-prev" src="images/chevron-left-solid.svg"></div>',
-        nextArrow: '<div class="slick-next"><img class="speakers__carousel--btn-next" src="images/chevron-right-solid.svg"></div>',
-        responsive: [
-            {
-              breakpoint: 1200,
-              settings: {
-                slidesToShow: 2
-              }
-            },
-            {
-                breakpoint: 991,
-                settings: {
-                    slidesToShow: 2,
-                    centerPadding: '80px 0px 0px',
-                    }
-             },
-             {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1,
-                    centerMode: false,
-                    }
-             },
-        ]
-      });
-
-    //Carousel image handler
-    $('.speakers__tile').click(function(event){
-        var id;
-        if($(event.target).parent().attr('data-slick-index') <'0'){
-            id = data.speakers.info.length + Number($(event.target).parent().attr('data-slick-index'));
-        }else if(Number($(event.target).parent().attr('data-slick-index')) ==='data.speakers.info.length'){
-            id = $(event.target).parent().attr('data-slick-index') - data.speakers.info.length;            
-        }else{
-            id = $(event.target).attr('id');
+  //Slick slider
+  $(".speakers__carousel").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: "40px 0px 0px",
+    prevArrow:
+      '<div class="slick-prev"><img class="speakers__carousel--btn-prev" src="images/chevron-left-solid.svg"></div>',
+    nextArrow:
+      '<div class="slick-next"><img class="speakers__carousel--btn-next" src="images/chevron-right-solid.svg"></div>',
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2
         }
-        console.log(id);
-    })
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 2,
+          centerPadding: "80px 0px 0px"
+        }
+      },
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 1,
+          centerMode: false
+        }
+      }
+    ]
+  });
+
+  //Carousel image handler
+  $(".speakers__tile").click(function(event) {
+    var dataId = $(event.target).attr("data-id");
+    $("#speaker-image").css(
+      "background-image",
+      `url(${data.speakers.info[dataId].image})`
+    );
+    $("#speaker-name").text(data.speakers.info[dataId].name);
+    $("#speaker-info").text(data.speakers.info[dataId].position);
+    console.log(dataId);
+  });
+
+  //Ticker
+  var ticker = $(".location__string--text");
+  $.fn.multiply = function(numCopies) {
+    var newElements = this.clone();
+    for (var i = 1; i < numCopies; i++) {
+      newElements = newElements.add(this.clone());
+    }
+    return newElements;
+  };
+  ticker.css({ overflow: "hidden", width: "100%" });
+  ticker.wrapInner("<span>");
+  ticker
+    .find("span")
+    .css({
+      width: "50%",
+      display: "inline-block",
+      "text-align": "center",
+      "font-weight": "bold"
+    });
+  ticker.append(ticker.find("span").multiply(1));
+  ticker.wrapInner("<div>");
+  ticker.find("div").css("width", "200%");
+  var reset = function() {
+    $(this).css("margin-left", "0%");
+    $(this).animate({ "margin-left": "-100%" }, 8000, "linear", reset);
+  };
+  reset.call(ticker.find("div"));
+
+  //Scroll
+  $('a[href^="#"], *[data-href^="#"]').on("click", function(e) {
+    e.preventDefault();
+    var t = 1000;
+    var d = $(this).attr("data-href")
+      ? $(this).attr("data-href")
+      : $(this).attr("href");
+    $("html,body")
+      .stop()
+      .animate({ scrollTop: $(d).offset().top - 100 }, t);
+  });
 })();
